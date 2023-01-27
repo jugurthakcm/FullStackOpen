1ade321c7b7a05c054a15395680e2d486b67bb47
jugurthakcm <49955338+jugurthakcm@users.noreply.github.com>
2023-01-27 13:15:05 +0000

    Add - feedback increment function

---
+++ unicafe/src/App.js
@@ -0,0 +1,29 @@
+import {useState} from "react";
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="feedback">
+        <h1>Give feedback</h1>
+
+        <button onClick={() => setGood(good + 1)}>Good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+        <button onClick={() => setBad(bad + 1)}>Bad</button>
+      </div>
+
+      <div className="results">
+        <h1>statistics</h1>
+
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
