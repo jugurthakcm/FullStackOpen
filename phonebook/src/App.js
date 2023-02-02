3142e85ba619096f436e5783543133370b38737a
jugurthakcm <49955338+jugurthakcm@users.noreply.github.com>
2023-02-02 15:59:50 +0000

    add - add person to numbers feature

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,5 +1,32 @@
-function App() {
-  return <div className="App"></div>;
-}
+import {useState} from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{name: "Arto Hellas"}]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPersons([...persons, {name: newName}]);
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
 
 export default App;
