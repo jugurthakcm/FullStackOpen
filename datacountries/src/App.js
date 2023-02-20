208e229e11cd3045a59281420ae197aa02f7e3a8
jugurthakcm <49955338+jugurthakcm@users.noreply.github.com>
2023-02-20 16:30:19 +0000

    fetch countries according to filter

--- datacountries/src/App.js
+++ datacountries/src/App.js
@@ -1,5 +1,30 @@
+import {useState, useEffect} from "react";
+import axios from "axios";
+
 function App() {
-  return <div className="App"></div>;
+  const [name, setName] = useState("");
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    return () => {
+      name &&
+        axios
+          .get(`https://restcountries.com/v3.1/name/${name}`)
+          .then((res) => setCountries(res.data));
+    };
+  }, [name]);
+
+  return (
+    <div className="App">
+      find countries
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      {countries.length < 10 ? (
+        countries.map((c) => <p key={c.area}>{c.name.common}</p>)
+      ) : (
+        <p>Too many countries to display</p>
+      )}
+    </div>
+  );
 }
 
 export default App;
