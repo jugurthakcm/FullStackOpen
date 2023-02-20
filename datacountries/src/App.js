61f50214a55a9f552b4c765f6d0a52f9b93c56c7
jugurthakcm <49955338+jugurthakcm@users.noreply.github.com>
2023-02-20 16:55:33 +0000

    add data when countru is displayed

--- datacountries/src/App.js
+++ datacountries/src/App.js
@@ -18,10 +18,34 @@ function App() {
     <div className="App">
       find countries
       <input value={name} onChange={(e) => setName(e.target.value)} />
-      {countries.length < 10 ? (
-        countries.map((c) => <p key={c.area}>{c.name.common}</p>)
-      ) : (
+      {countries.length === 1 &&
+        countries.map((c) => {
+          return (
+            <div key={c.area}>
+              <h2>{c.name.common}</h2>
+
+              <p>Capital: {c.capital[0]}</p>
+              <p>Area: {c.area}</p>
+
+              <strong>languages:</strong>
+
+              <ul>
+                {Object.keys(c.languages).map((key) => (
+                  <p>{c.languages[key]}</p>
+                ))}
+              </ul>
+
+              <br />
+
+              <img src={c.flags.png} alt="flag" width={100} />
+            </div>
+          );
+        })}
+      {countries.length > 10 ? (
         <p>Too many countries to display</p>
+      ) : (
+        countries.length > 1 &&
+        countries.map((c) => <p key={c.area}>{c.name.common}</p>)
       )}
     </div>
   );
