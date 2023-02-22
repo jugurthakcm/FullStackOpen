e35283e650cd3e869b31b9fff309b5c056aea479
jugurthakcm <49955338+jugurthakcm@users.noreply.github.com>
2023-02-22 04:18:16 +0000

    display info and time of request in /info

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -28,6 +28,12 @@ app.get("/api/persons", (req, res) => {
   return res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  const date = new Date();
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+  <p>${date}</p>`);
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
