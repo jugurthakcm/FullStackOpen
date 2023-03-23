430e5fa1509cac2ed905770b1269683bfc73d4b1
jugurthakcm <49955338+jugurthakcm@users.noreply.github.com>
2023-03-23 15:58:40 +0000

    most liked blog function and test it

--- blog/utils/list_helper.js
+++ blog/utils/list_helper.js
@@ -6,4 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, val) => acc + val.likes, 0);
 };
 
-module.exports = {dummy, totalLikes};
+
+const favoriteBlog = (blogs) => {
+  const {title, author, likes} = blogs.reduce((acc, curr) => {
+    return acc.likes > curr.likes ? acc : curr
+  })
+
+  return {title, author, likes}
+}
+module.exports = {dummy, totalLikes, favoriteBlog};
