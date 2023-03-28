4562d65b7acf4871670bf897e62cd263f4769e1f
jugurthakcm <49955338+jugurthakcm@users.noreply.github.com>
2023-03-28 19:43:45 +0000

    Refactoring tests

--- blog/tests/blogs_api.test.js
+++ blog/tests/blogs_api.test.js
@@ -31,62 +31,66 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-// Test if /api/blogs return correct amount of blogs
-test("test if /api/blogs return correct amount of blogs", async () => {
-  const response = await api.get("/api/blogs");
+describe("Fetching Blogs", () => {
+  // Test if /api/blogs return correct amount of blogs
+  test("test if /api/blogs return correct amount of blogs", async () => {
+    const response = await api.get("/api/blogs");
 
-  expect(response._body).toHaveLength(initialBlogs.length);
+    expect(response._body).toHaveLength(initialBlogs.length);
+  });
+
+  // Test if the response data are returned with an "id"
+  test("test if response are returned with id", async () => {
+    const response = await api.get("/api/blogs");
+
+    response._body.forEach((e) => expect(e.id).toBeDefined());
+  });
 });
 
-// Test if the response data are returned with an "id"
-test("test if response are returned with id", async () => {
-  const response = await api.get("/api/blogs");
+describe("Adding New Blogs", () => {
+  // Test if new blogs are added properly
+  test("test if new blogs are added properly", async () => {
+    const newBlogObject = {
+      title: "New Blog",
+      author: "New Author",
+      url: "New Url",
+      likes: 7,
+    };
 
-  response._body.forEach((e) => expect(e.id).toBeDefined());
-});
+    await api.post("/api/blogs").send(newBlogObject);
+    const response = await api.get("/api/blogs");
 
-// Test if new blogs are added properly
-test("test if new blogs are added properly", async () => {
-  const newBlogObject = {
-    title: "New Blog",
-    author: "New Author",
-    url: "New Url",
-    likes: 7,
-  };
+    expect(response._body).toHaveLength(initialBlogs.length + 1);
 
-  await api.post("/api/blogs").send(newBlogObject);
-  const response = await api.get("/api/blogs");
+    const blogsTitle = response._body.map((blog) => blog.title);
 
-  expect(response._body).toHaveLength(initialBlogs.length + 1);
+    expect(blogsTitle).toContain("New Blog");
+  });
 
-  const blogsTitle = response._body.map((blog) => blog.title);
+  // Test if likes property is defaulted to 0
+  test("Test if likes property is defaulted to 0", async () => {
+    const newBlogObject = {
+      title: "New Blog",
+      author: "New Author",
+      url: "New Url",
+    };
 
-  expect(blogsTitle).toContain("New Blog");
-});
+    const response = await api.post("/api/blogs").send(newBlogObject);
 
-// Test if likes property is defaulted to 0
-test("Test if likes property is defaulted to 0", async () => {
-  const newBlogObject = {
-    title: "New Blog",
-    author: "New Author",
-    url: "New Url",
-  };
+    expect(response._body.likes).toBe(0);
+  });
 
-  const response = await api.post("/api/blogs").send(newBlogObject);
+  // Test if title or url is missing 400 Bad Request is sent
+  test("Test if title or url is missing 400 Bad Request is sent", async () => {
+    const newBlogObject = {
+      author: "New Author",
+      title: "nwddsd",
+      likes: 12,
+    };
+    const response = await api.post("/api/blogs").send(newBlogObject);
 
-  expect(response._body.likes).toBe(0);
-});
-
-// Test if title or url is missing 400 Bad Request is sent
-test("Test if title or url is missing 400 Bad Request is sent", async () => {
-  const newBlogObject = {
-    author: "New Author",
-    title: "nwddsd",
-    likes: 12,
-  };
-  const response = await api.post("/api/blogs").send(newBlogObject);
-
-  expect(response.status).toBe(400);
+    expect(response.status).toBe(400);
+  });
 });
 
 // Close connection to the DB
