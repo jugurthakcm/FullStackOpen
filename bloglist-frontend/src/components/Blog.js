128ae915f3d0f45e833e2a720e5309149d1a30bb
jugurthakcm <49955338+jugurthakcm@users.noreply.github.com>
2023-04-06 14:31:30 +0000

    toggling view of blog details

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from "react";
 
-export default Blog
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const [toggleView, setToggleView] = useState(false);
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}{" "}
+        <button onClick={() => setToggleView(!toggleView)}>
+          {!toggleView ? "View" : "Hide"}
+        </button>
+        {toggleView ? (
+          <div>
+            <p>{blog.url}</p>
+            <p>
+              {blog.likes} <button>Like</button>
+            </p>
+            <p>{blog.author}</p>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
