186412e27c0376193f5c8935694df1786499af0f
Jugurtha <jugurthakacimi11@gmail.com>
2023-04-15 19:02:17 -0500

    fetching anecdotes using react-query

--- query-anecdotes/src/index.js
+++ query-anecdotes/src/index.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App'
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
